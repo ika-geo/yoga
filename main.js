f29aa2f3db4ddab78904a15d7130f7d25cb9c1db
Your Name <you@example.com>
2021-07-01 21:03:52 +0400

    + fixed menu

--- main.js
+++ main.js
@@ -7,3 +7,27 @@ $menuBtn.addEventListener("click", function (){
     $menuBtn.classList.toggle("active")
     $headerMenu.classList.toggle("active")
 })
+
+
+// fixed header
+
+let $headerTop = document.querySelector(".header__top")
+
+window.addEventListener("scroll", function (){
+    if (window.scrollY>50&&window.innerWidth>768){
+        $headerTop.classList.add("fixed")
+    }
+    else {
+        $headerTop.classList.remove("fixed")
+    }
+})
+
+
+window.addEventListener("resize", function (){
+    if (window.innerWidth>768&&window.scrollY>50){
+        $headerTop.classList.add("fixed")
+    }
+    else {
+        $headerTop.classList.remove("fixed")
+    }
+})
